e182173937ea0a01c22357b3950766fe119a632e
VladPanovS <34751539+VladPanovS@users.noreply.github.com>
2018-05-03 18:10:15 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
+    //animation
+             var wow = new WOW({
+               mobile: false,
+             });
              new WOW().init();
+    //burgre menu
+             $(".burger").click(function(){
+             $(".burger-line").toggleClass("line-active")
+             $(".burger").toggleClass("rotate")
+             $(".menu").toggleClass("open");
+         });
 });
